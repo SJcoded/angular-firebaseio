9234524f7e011b78650150b7627e4e7d2cdf260c
SJ coded <who@sjcoded.com>
2021-12-11 17:23:19 +1100

    clean slate

--- src/app/bigger.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BiggerDirective } from './bigger.directive';
-
-describe('BiggerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BiggerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/colour.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ColourPipe } from './colour.pipe';
-
-describe('ColourPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ColourPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

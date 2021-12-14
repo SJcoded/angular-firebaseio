e52314a345930d029eecaf294b42d75a6391f2d8
SJ coded <who@sjcoded.com>
2021-12-14 17:08:26 +1100

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'scholarmode-projects',
+    appId: '1:706109410758:web:b7c560920b127e8c1876c7',
+    storageBucket: 'scholarmode-projects.appspot.com',
+    apiKey: 'AIzaSyDoLvhU9gK7SLneiHRwU4mYcBS_5zF241Y',
+    authDomain: 'scholarmode-projects.firebaseapp.com',
+    messagingSenderId: '706109410758',
+    measurementId: 'G-CL0XZ5PGHB',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'scholarmode-projects',
+    appId: '1:706109410758:web:b7c560920b127e8c1876c7',
+    storageBucket: 'scholarmode-projects.appspot.com',
+    apiKey: 'AIzaSyDoLvhU9gK7SLneiHRwU4mYcBS_5zF241Y',
+    authDomain: 'scholarmode-projects.firebaseapp.com',
+    messagingSenderId: '706109410758',
+    measurementId: 'G-CL0XZ5PGHB',
+  },
+  
 };
 
 /*
